cc53e0a5d60d7a24aa9d84bd296292091b9420d3
mng16 <mairi.ng.16@ucl.ac.uk>
2017-02-24 14:04:21 +0000

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,27 @@
+<?php
+  // 1. Create a database connection
+    // 1b. Connecting with MAMP
+    // Instructions from "http://localhost:8888/MAMP/?language=English" for PHP>=5.6.x
+    $user = 'root';
+    $password = 'root';
+    $db = 'echochamber';
+    $host = 'localhost';
+    $port = 8889;
+    
+    $link = mysqli_init();
+    $success = mysqli_real_connect(    
+        $link, 
+        $host,
+        $user,
+        $password,
+        $db,
+        $port
+    );
+    
+  if(mysqli_connect_errno()) {
+    die("Database connection failed: " . 
+         mysqli_connect_error() . 
+         " (" . mysqli_connect_errno() . ")"
+    );
+  }
+?>
