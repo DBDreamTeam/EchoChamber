0edc5241761cead149e35db02cbab14cedd58d41
Marisa <MarisaAlina@users.noreply.github.com>
2017-02-27 12:25:04 +0100

    Added fail message & tied to new login-destination
    Added fail message & tied to new login-destination.php

---
+++ Echo Chamber (WIP)/index.php
@@ -0,0 +1,137 @@
+<?php
+include 'connect.php';
+session_start();
+?>
+
+<!-- DOCTYPE -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Echo Chamber Login</title>
+    <meta charset="utf-8">
+    <meta name="author" content="DBDreamTeam">
+    <meta name="viewport" content="width=device-width, intial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+    <!-- Fonts -->
+    <link rel='stylesheet' type='text/css'
+        href='https://fonts.googleapis.com/css?family=Lato:400,700,900,300'>
+    <link rel='stylesheet' type='text/css'
+        href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic'>
+    <link rel='stylesheet' type='text/css'
+        href='https://fonts.googleapis.com/css?family=Raleway:400,300,600,700,900'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+
+<!-- Wrapper Start -->
+<div class="wrapper" id="wrapper">
+
+<!-- Header -->
+<header>
+<div class="jumbotron jumbotron-fluid" id="banner">
+<div class="parallax test-centre" style="background-image: url(img/cover.jpg);">
+<div class="parallax-pattern-overlay">
+    <div class="container text-center" style="height: 60px; padding-top=170px;">
+        <a href="#"><img id="site-title" src="img/ECLogo1.png" alt="logo"
+            style="display:block; height:70px; display:inline; position:relative; left:-20px; top:-10px"/></a>
+        <h1 style="display:inline">EchoChamber</h1>
+        <h6 style="display:inline">a social network for people just like you.<h6>
+    </div>
+</div>
+</div>
+</header>
+
+<!-- About Us Section -->
+<section class="aboutus" id="aboutus">
+<div class="container">
+    <div class="row" style="padding-top:30px">
+        <div class="col-md-6">
+            <!--
+            <div class="heading text-center">
+            <img class="dividerline" src="img/sep.png" alt="">
+            <h2>About EchoChamber</h2>
+            <img class="dividerline" src="img/sep.png" alt="">
+            -->
+            <p> <b>EchoChamber</b> is the social network for the modern day.
+                Here, you can avoid seeing any views with which you disagree,
+                and bask in just how fabulously <b>right</b> you are about
+                everything. Join groups of like-minded people and while
+                away your days patting each other on the back, reaffirming
+                the views you came here with, and generally just reminding
+                yourself how superior you are to all those other idiots out
+                there. <b>Have fun!</b>
+            <p>
+        </div>
+        <div class="col-md-6 text-right">
+            <p style="position:relative; right:10px"><b>Log In</b></p>
+            <form action="login-destination.php" method="post">
+                <label>
+                    Username:
+                    <input type="Username" name="Username">
+                </label>
+                <label>
+                    Password:
+                    <input type="Password" name="Password">
+                </label>
+                <label>
+                    <input type="submit" value="Log In">
+                </label> 
+                <div id="errorMsg">
+            <?php if(!empty($_SESSION['errorMsg'])) { 
+                    echo $_SESSION['errorMsg']; } ?>
+                </div>
+            <?php unset($_SESSION['errorMsg']); ?>
+            </form>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3">
+        </div>
+        <div class="col-md-3">
+        </div>
+        <div class="col-md-3">
+        </div>
+        <div class="col-md-3">
+        </div>
+    </div>
+</div>
+</section>
+
+<!-- Features Section -->
+<section class="features" id="features">
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+        </div>
+    </div>
+</div>
+</section>
+
+<!-- Contact Section -->
+<section class="contact" id="contact">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+        </div>
+    </div>
+</div>
+</section>
+
+<!-- Footer -->
+<section class="footer" id="footer">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+        </div>
+    </div>
+</div>
+</section>
+
+<!-- Wrapper End -->
+</div>
