f6a9750e350331ea1e41d46900bf5afabdeec50a
cmabe <cmabel.hk@gmail.com>
2017-03-11 12:49:44 +0000

    Add files via upload

--- Echo Chamber (WIP)/header.php
+++ Echo Chamber (WIP)/header.php
@@ -1,9 +1,7 @@
-<?php  
-
+<?php
 require 'connect.php';
-
 ?>
-	
+
 
 <div id = 'title_bar'>
 	<li><a href ='friend.php'>Home</a><li>
@@ -11,8 +9,4 @@ require 'connect.php';
 	<li><a href = 'fakeUser.php'> Login - Add/Remove Friend</a></li>
 	<li><a href = 'Login_friendRequest.php'> Login - Friend Request</a></li>
 	<div class ='clear'></div>
-</div> 
-
-
-
-
+</div>
