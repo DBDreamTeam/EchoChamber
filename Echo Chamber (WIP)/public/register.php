f927570c85d60bac7d9961c4cbd548e1fd8132bf
Zak <tOOtl@users.noreply.github.com>
2017-03-13 11:16:01 +0000

    Style it out

--- Echo Chamber (WIP)/public/register.php
+++ Echo Chamber (WIP)/public/register.php
@@ -8,9 +8,9 @@
     <title>EchoChamber</title>
 
     <!-- Bootstrap -->
-    <link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
-    <link href="css/custom.css" rel="stylesheet" type="text/css">
+    <link href="../css/custom.css?v=<? echo time(); ?>" rel="stylesheet" type="text/css">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -72,6 +72,6 @@
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+    <script src="../bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
   </body>
 </html>
