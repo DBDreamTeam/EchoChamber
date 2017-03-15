27449788c6279d11573e5dca2e71557cfe13259e
mng16 <mairi.ng.16@ucl.ac.uk>
2017-03-15 12:04:39 +0000

    Upload photo functionality added

--- Echo Chamber (WIP)/process/blog.php
+++ Echo Chamber (WIP)/process/blog.php
@@ -2,60 +2,52 @@
 
 session_start();
 
-include("../includes/connect.php");
-include("../process/sentiments.php");
+include '../includes/connect.php';
+include '../includes/functions.php';
 
 $blogInput = $_POST["blogInput"];
+echo $blogInput;
 //get the BlogID of current profile
+$FriendUserID = $_SESSION["FriendUserID"];
 $LoggedUserID = $_SESSION["LoggedUserID"];
-$BlogID = $_POST["BlogID"];
+$BlogID = $_SESSION["BlogID"];
 $isGroup = $_SESSION["isGroup"];
 
+$picID = null;
+
+// if an image has been uploaded
+if(!empty($_FILES["uploadedimage"]["name"])){
+    // checks if album of name "Blog Pictures" already exists"
+    $doesAlbumExist = doesAlbumNameExist("Blog Pictures", $LoggedUserID, $link);
+    
+    if($doesAlbumExist == 1) {
+    // if a "Blog Pictures" album does exist, get the albumID and add the new blog picture to it
+    $albumID = getAlbumIDFromName('Blog Pictures', $LoggedUserID, $link);
+    $picID = insertImageNew($albumID, "uploadedimage", $link);
+    echo "Album exist <br>";
+    } else {
+    // if the album does not already exist, create it and add the picture to it
+    $albumID = insertAlbum('Blog Pictures', $LoggedUserID, 'Friends', $link);
+    $picID = insertImageNew($albumID, "uploadedImage", $link);
+    }
+}
+ 
+
 if ($isGroup == '0') {
-  header("Location: ../public/profile.php");
   echo "individual";
-  //echo '<script type="text/javascript">window.location = "http://localhost:8888/ECHOCHAMBER/public/profile.php";</script>';
+  echo '<script type="text/javascript">window.location = "http://localhost:8888/ECHOCHAMBER/public/profile.php";</script>';
 } else {
-  header("Location: ../public/groupBlog.php");
   echo "group";
-  //echo '<script type="text/javascript">window.location = "http://localhost:8888/groupBlog.php";</script>';
+  echo '<script type="text/javascript">window.location = "http://localhost:8888/groupBlog.php";</script>';
 }
 
-$insertBlog = "INSERT INTO posts (BlogID, text, AlbumID) VALUES ('$BlogID','$blogInput', NULL)";
-echo "<br>" . $insertBlog;
-if ($link->query($insertBlog) === TRUE) {
+$insertBlog = "INSERT INTO posts (BlogID, text, PictureID) VALUES ('$BlogID','$blogInput', $picID)";
+if ($link -> query($insertBlog) === TRUE) {
 } else {
   echo "Error: ". $insertBlog . "<br>" . $link->error;
 }
 
 
-
-// Update sentiments table with new data
-$text_from_posts_sql = "
-    SELECT text FROM posts
-        JOIN blog_wall
-        ON posts.BlogID = blog_wall.ID
-        WHERE blog_wall.IsGroup = 0
-        AND blog_wall.OwnerID = $LoggedUserID";
-$text_from_posts_result = $link->query($text_from_posts_sql);
-$text_from_posts_array = mysqli_fetch_array($text_from_posts_result, MYSQLI_NUM);
-$all_text_from_posts = implode(". ", $text_from_posts_array);
-
-$text_from_comments_sql = "
-    SELECT Text FROM comments
-        WHERE UserID = $LoggedUserID";
-$text_from_comments_result = $link->query($text_from_comments_sql);
-$text_from_comments_array = mysqli_fetch_array($text_from_comments_result, MYSQLI_NUM);
-$all_text_from_comments = implode(". ", $text_from_comments_array);
-
-$entity_sentiments = getEntitySentiments($all_text_from_posts . ". " . $all_text_from_comments);
-
-foreach ($entity_sentiments as $entity => $sentiments) {
-  $update_sentiments_sql = "
-      REPLACE INTO sentiments (UserID, Entity, Sentiment)
-          VALUES ($LoggedUserID, $entity, $sentiment)";
-  $link->query($entity_sentiments_sql);
-}
 
 
 ?>
